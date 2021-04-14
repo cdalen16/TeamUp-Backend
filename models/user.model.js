63421eb1487dea9b8cd54c52347a63d2e321e761
Campbell Dalen <cdalen@vt.edu>
2021-04-14 15:23:12 -0400

    Updated backend for user login and registration

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+//TODO: notice that User has evolved and now includes 'caloriegoal' and 'minutegoal'.
+//DONE
+const schema = new Schema({
+        username: { type: String, unique: true, required: true },
+        email: { type: String, unique: true, required: true },
+        hash: { type: String, required: true },
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        role: {type:String, required: true},
+        createdDate: { type: Date, default: Date.now },
+        caloriegoal: { type: Number, required: true, default: 2000 },
+        minutegoal: { type: Number, required: true, default: 65 },
+        avatarcolor: { type: String, required: true, default: "012157" }
+    }
+);
+
+schema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('User', schema);
